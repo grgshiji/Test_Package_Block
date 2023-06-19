09dc4679b4572e39e1a524b1411c8be4a4f2f9db
grgshiji <shiji@neoito.com>
2023-06-19 23:12:35 +0530

    [commitHash:76a20ef]

---
+++ src/remote/Member_UI_Elements_Block.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+
+export const Member_ui_elements_block = () => {
+  return (
+   <p>Hello from Member_UI_Elements_Block </p>
+  )
+}
+export default Member_ui_elements_block
